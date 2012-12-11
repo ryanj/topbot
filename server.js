52d760a6dc092188a3703d334bf72fd0b2d03079
ryanj <dontspamryan@gmail.com>
2012-12-11 15:56:52 -0800

    switching from npm lib "irc" to "jerk"

--- server.js
+++ server.js
@@ -1,35 +1,45 @@
 #!/bin/env node
 //  OpenShift sample Node application
-var express = require('express');
+//var express = require('express');
+var jerk = require( 'jerk' )
+//var irc = require('irc');
 
-var irc = require('irc');
-var irc_client = new irc.Client('chat.freenode.net', 'bad_sudobot', {
-    channels: ['#sudoroom'],
-});
+var bot_name = 'bad_sudobot';
+var irc_options =
+  { server: 'irc.freenode.net'
+  , nick: bot_name
+  , channels: [ '#sudoroom' ]
+  }
 
-irc_client.addListener('message', function(from, to, message) {
+jerk( function( j ) {
+  j.watch_for( 'soup', function( message ) {
+    message.say( message.user + ': soup is good food!' )
+  })
 
-        if(message.indexOf('yarr') > -1) {
-                irc_client.say(to, 'Arrr Matey!');
-        }
-});
+  j.watch_for( /^(.+) are silly$/, function( message ) {
+    message.say( message.user + ': ' + message.match_data[1] + ' are NOT SILLY. Don\'t joke!' )
+  })
 
-irc_client.addListener('message', function(from, to, message) {
+  j.watch_for( /^<3$/, function( message ) {
+    message.say( message.user + ': ' + ' <3' )
+  })
 
-    if(message.indexOf('<3') > -1) {
-        irc_client.say(to, '<3');
-    }
-});
+  j.watch_for( /^yarr$/, function( message ) {
+    message.say( message.user + ': ' + 'Arrr Matey!' )
+  })
 
-irc_client.addListener('message', function(from, to, message) {
-    if(message == 'randomize') {
-        irc_client.say(to, Math.round(Math.random() * 10));
-    }
-});
+}).connect( irc_options )
 
-irc_client.addListener('pm', function (from, message) {
-    if( message.slice(0,4) == 'say '){
-        irc_client.say('#sudoroom', message.slice(4));
-    }
-    console.log(from + ' => ME: ' + message);
-});
+//irc_client.addListener('pm', function (from, message) {
+//    if( message.slice(0,4) == 'say '){
+//        irc_client.say('#sudoroom', message.slice(4));
+//    }
+//    console.log(from + ' => ME: ' + message);
+//});
+//
+//irc_client.addListener('connect', function() {
+//    console.log(bot_name + 'connecting...');
+//});
+//irc_client.addListener('error', function(message) {
+//    console.log('error: ', message);
+//});
