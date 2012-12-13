12215f7e92217be2bb444260cc174e9c10698fc7
ryanj <ryan.jarvinen@gmail.com>
2012-12-12 18:20:58 -0800

    removing some code related to "jerk" irc libs

--- server.js
+++ server.js
@@ -34,34 +34,17 @@ bot.addListener('pm', function (from, message) {
     console.log(from + ' => ME: ' + message);
 });
 
-//var irc_options =
-//  { server: 'chat.freenode.net'
-//  , nick: 'topbot'
-//  , channels: [ '#sudoroom', '#botzoo' ]
-//  }
-//
 //var sudobot = jerk( function( j ) {
 //  j.watch_for( 'soup', function( message ) {
 //    message.say( message.user + ': soup is good food!' )
 //  })
-//
 //  j.watch_for( /^(.+) are silly$/, function( message ) {
 //    message.say( message.user + ': ' + message.match_data[1] + ' are NOT SILLY. Don\'t joke!' )
 //  })
-//
-//  j.watch_for( /^<3$/, function( message ) {
-//    message.say( message.user + ': ' + ' <3' )
-//  })
-//
 //  j.watch_for( /^say (.+)$/, function( message ) {
 //    if(message.source[1] !== "#"){
 //      message.msg( message.user + ': you got it!' )
 //      sudobot.say( '#sudoroom', message.match_data[1] )
 //    }
 //  })
-//
-//  j.watch_for( /^yarr$/, function( message ) {
-//    message.say( message.user + ': ' + 'Arrr Matey!' )
-//  })
-//
 //}).connect( irc_options )
