524a6c972c5420d3f89d38b1d92095f0714bab12
ryanj <ryan.jarvinen@gmail.com>
2012-12-12 18:15:11 -0800

    testing a switch back to npm's "irc"

--- server.js
+++ server.js
@@ -2,37 +2,66 @@
 //  OpenShift sample Node application
 //var express = require('express');
 
-var jerk = require( 'jerk' )
+var irc = require('irc');
+var bot = new irc.Client('chat.freenode.net', 'topbot', {
+    channels: ['#sudoroom', '#botwar', '#botzoo'],
+    port: 8001,
+    debug: true
+});
 
-var irc_options =
-  { server: 'chat.freenode.net'
-  , nick: 'topbot'
-  , channels: [ '#sudoroom', '#botzoo' ]
-  , port: 8001
-  }
+bot.addListener('message', function(from, to, message) {
+   if(message.indexOf('yarr') > -1) {
+        bot.say(to, 'Arrr Matey!');
+   }
+});
 
-var sudobot = jerk( function( j ) {
-  j.watch_for( 'soup', function( message ) {
-    message.say( message.user + ': soup is good food!' )
-  })
-
-  j.watch_for( /^(.+) are silly$/, function( message ) {
-    message.say( message.user + ': ' + message.match_data[1] + ' are NOT SILLY. Don\'t joke!' )
-  })
-
-  j.watch_for( /^<3$/, function( message ) {
-    message.say( message.user + ': ' + ' <3' )
-  })
-
-  j.watch_for( /^say (.+)$/, function( message ) {
-    if(message.source[1] !== "#"){
-      message.msg( message.user + ': you got it!' )
-      sudobot.say( '#sudoroom', message.match_data[1] )
+bot.addListener('message', function(from, to, message) {
+    if(message.indexOf('<3') > -1) {
+        bot.say(to, '<3');
     }
-  })
+});
 
-  j.watch_for( /^yarr$/, function( message ) {
-    message.say( message.user + ': ' + 'Arrr Matey!' )
-  })
+bot.addListener('message', function(from, to, message) {
+    if(message == 'randomize') {
+        bot.say(to, Math.round(Math.random() * 10));
+    }
+});
 
-}).connect( irc_options )
+bot.addListener('pm', function (from, message) {
+    if( message.slice(0,4) == 'say '){
+        bot.say('#sudoroom', message.slice(4));
+    }
+    console.log(from + ' => ME: ' + message);
+});
+
+//var irc_options =
+//  { server: 'chat.freenode.net'
+//  , nick: 'topbot'
+//  , channels: [ '#sudoroom', '#botzoo' ]
+//  }
+//
+//var sudobot = jerk( function( j ) {
+//  j.watch_for( 'soup', function( message ) {
+//    message.say( message.user + ': soup is good food!' )
+//  })
+//
+//  j.watch_for( /^(.+) are silly$/, function( message ) {
+//    message.say( message.user + ': ' + message.match_data[1] + ' are NOT SILLY. Don\'t joke!' )
+//  })
+//
+//  j.watch_for( /^<3$/, function( message ) {
+//    message.say( message.user + ': ' + ' <3' )
+//  })
+//
+//  j.watch_for( /^say (.+)$/, function( message ) {
+//    if(message.source[1] !== "#"){
+//      message.msg( message.user + ': you got it!' )
+//      sudobot.say( '#sudoroom', message.match_data[1] )
+//    }
+//  })
+//
+//  j.watch_for( /^yarr$/, function( message ) {
+//    message.say( message.user + ': ' + 'Arrr Matey!' )
+//  })
+//
+//}).connect( irc_options )
